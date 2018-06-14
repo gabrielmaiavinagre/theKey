74ba6716b0cb2e473f97e9cc948233f1c30a07cc
Gabriel Maia <gabriel.maia@radixeng.com.br>
2018-06-14 20:30:25 -0300

    #fazendo a implementacao do keychain e fazendo a codificacao e descodificacao do objeto Secret para NSData e vice versa

--- TheKey/Controller/DataManager.swift
+++ TheKey/Controller/DataManager.swift
@@ -6,4 +6,63 @@
 //  Copyright © 2018 gabrielVinagre. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import KeychainSwift
+
+class DataManager {
+    
+    private static var allSecrets = [Secret]()
+    
+    class func saveData(username: String, secret: Secret) {
+        
+        if allSecrets.count == 0 {
+            getAllData(username: username)
+        }
+        
+        if findSecret(secret: secret) == nil {
+        allSecrets.append(secret)
+        let data = transformToData(secret: allSecrets)
+        let keychain = KeychainSwift()
+        keychain.set(data, forKey: username)
+        }
+    }
+    
+    class func deleteSecret(username: String, secret: Secret) {
+        
+        allSecrets = allSecrets.filter { $0 != secret }
+        let data = transformToData(secret: allSecrets)
+        let keychain = KeychainSwift()
+        keychain.set(data, forKey: username)
+
+    }
+    
+    class func transformToData(secret: [Secret])-> Data {
+        let data = NSKeyedArchiver.archivedData(withRootObject: allSecrets)
+        return data
+    }
+
+    class func transformToObject(data: Data) -> [Secret] {
+        return NSKeyedUnarchiver.unarchiveObject(with: data) as? [Secret] ?? [Secret]()
+    }
+    
+    class func findSecret(secret: Secret)-> Secret? {
+        
+        for element in allSecrets {
+            
+            if element == secret {
+                return secret
+            }
+        }
+        return nil
+    }
+
+    
+    class func getAllData(username: String)-> [Secret] {
+        let keychain = KeychainSwift()
+        if let arrayOfData = keychain.getData(username) as? Data {
+            let arrayConverted: [Secret] = transformToObject(data: arrayOfData)
+        return arrayConverted
+        }
+       return [Secret]()
+    }
+}
--- TheKey/Model/Secret.swift
+++ TheKey/Model/Secret.swift
@@ -8,17 +8,29 @@
 
 import Foundation
 
-class Secret {
+class Secret: NSObject, NSCoding {
     
-    private var name: String
-    private var username: String
-    private var password: String
+    private var name: String = ""
+    private var username: String = ""
+    private var password: String = ""
     
     init(name: String, username: String, password: String) {
         
         self.name = name
         self.username = username
         self.password = password
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObject(forKey: "name") as! String
+        self.username = aDecoder.decodeObject(forKey: "username") as! String
+        self.password = aDecoder.decodeObject(forKey: "password") as! String
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(username, forKey: "username")
+        aCoder.encode(password, forKey: "password")
     }
     
     func getSiteName() -> String {
