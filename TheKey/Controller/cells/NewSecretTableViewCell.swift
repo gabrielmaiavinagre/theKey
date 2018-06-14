adfba96bc31a5b9c76868c9084714c452e4fcada
GabrielVinagre <gabrielmaiavinagre@gmail.com>
2018-06-14 00:41:17 -0300

    #inserindo imagens e novos view controllers

---
+++ TheKey/Controller/cells/NewSecretTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewSecretTableViewCell.swift
+//  TheKey
+//
+//  Created by Gabriel Maia Vinagre Costa on 13/06/18.
+//  Copyright © 2018 gabrielVinagre. All rights reserved.
+//
+
+import UIKit
+
+class NewSecretTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
